46d6a43e415bbe11cf0174441776238df9180b3b
Jack Rose <jacqueline.rose.21@cnu.edu>
2025-02-10 12:47:58 -0500

    Tutorial for drawing images from image files

--- main.lua
+++ main.lua
@@ -1,3 +1,9 @@
+
+
+function love.load()
+	bunny = love.graphics.newImage("MyBunny.png")
+end
 function love.draw()
+	love.graphics.draw(bunny, 0, 0)
     love.graphics.print("Hello World", 400, 300)
 end
